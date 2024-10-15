3274ee0296baf32cb733d7849088c81c3877bf0b
Dilshad <helloshad3@gmail.com>
2024-10-15 12:15:33 +0530

    Project Setup

---
+++ app/components/Navigation.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+function Navigation() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link href={ }></Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navigation
