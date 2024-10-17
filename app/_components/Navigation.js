a7d752ed974024a85edc2223fa12b1a1cab3134b
Dilshad <helloshad3@gmail.com>
2024-10-17 15:45:52 +0530

    Adding nested Layout

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -5,11 +5,6 @@ function Navigation() {
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
         <li>
-          <Link href="/" className="hover:text-accent-400 transition-colors">
-            Home
-          </Link>
-        </li>
-        <li>
           <Link
             href="cabins"
             className="hover:text-accent-400 transition-colors"
@@ -17,20 +12,21 @@ function Navigation() {
             Cabins
           </Link>
         </li>
-        <li>
-          <Link
-            href="account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Account
-          </Link>
-        </li>
+
         <li>
           <Link
             href="about"
             className="hover:text-accent-400 transition-colors"
           >
             About
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="account"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Guest Area
           </Link>
         </li>
       </ul>
--- app/account/page.js
+++ app/account/page.js
@@ -4,7 +4,9 @@ export const metadata = {
 export default function Page() {
   return (
     <>
-      <h1>Inside acount page</h1>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Jones
+      </h2>
     </>
   );
 }
